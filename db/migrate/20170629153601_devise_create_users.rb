d1c096bf41f6bd3bd7d09a44aa47be805c17f9db
Skyridge Devcamp <skyridge@devcamp.com>
2017-06-29 16:23:23 +0000

    Custimized registration, login, and logout routes

--- db/migrate/20170629153601_devise_create_users.rb
+++ db/migrate/20170629153601_devise_create_users.rb
@@ -4,6 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      
+      ##Custom fields
+      t.string :name
 
       ## Recoverable
       t.string   :reset_password_token
