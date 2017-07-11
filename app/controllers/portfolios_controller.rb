3f9efaae933da70ed8022d8fe458b341e6e62233
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-11 16:29:14 +0000

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
     def index
         @portfolio_item = Portfolio.all
@@ -26,12 +27,10 @@ class PortfoliosController < ApplicationController
     end
     
     def edit
-        @portfolio_item = Portfolio.find(params[:id])
         3.times { @portfolio_item.technologies.build }
     end
     
     def update
-        @portfolio_item = Portfolio.find(params[:id])
         
         respond_to do |format|
           if @portfolio_item.update(portfolio_params)
@@ -43,12 +42,9 @@ class PortfoliosController < ApplicationController
     end
     
     def show
-      @portfolio_item = Portfolio.find(params[:id])
     end
     
     def destroy
-       @portfolio_item = Portfolio.find(params[:id])
-       
       @portfolio_item.destroy
       respond_to do |format|
       format.html { redirect_to portfolios_url, notice: 'Record was Removed' }
@@ -64,5 +60,9 @@ class PortfoliosController < ApplicationController
                                         technologies_attributes: [:name]
                                         )
     
+    end
+    
+    def set_portfolio_item
+      @portfolio_item = Portfolio.find(params[:id])
     end
 end
