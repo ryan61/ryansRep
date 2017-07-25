fe2d46c7009634ca31349a04b1acdd697b8aa48e
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-25 14:29:48 +0000

    Clean up garbage

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -65,9 +65,7 @@ class PortfoliosController < ApplicationController
     def portfolio_params
       params.require(:portfolio).permit(:title, 
                                         :subtitle, 
-                                        :body,
-                                        :main_image,
-                                        :thumb_image,
+                                        :body, 
                                         technologies_attributes: [:name]
                                         )
     
