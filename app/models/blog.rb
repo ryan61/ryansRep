1e96b8325eb636cc97b1443598e090f8a8aa3bfd
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-27 21:36:34 +0000

    Implemented redis library and created comment generator

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -5,5 +5,14 @@ class Blog < ApplicationRecord
     
     validates_presence_of :title, :body
     
-    #belongs_to :topic
+    belongs_to :topic
+    has_many :comments, dependent: :destroy
+    
+    def self.special blogs
+         all
+    end
+    
+    def self.featured_blogs
+         limit(2)
+    end
 end
