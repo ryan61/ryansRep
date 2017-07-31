b015048bfba9f497d680a010119aa96d3363232e
Skyridge Devcamp <skyridge@devcamp.com>
2017-07-31 17:11:05 +0000

    Created a comment broadcast, configured the websocket route, and updated the comment model with a callback

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :blog
   
   validates :content, presence: true, length: { minimum: 5, maximum: 1000 }
-end
+  
+  after_create_commit { CommentBroadcastJob.perfom_later(self) }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :comments
   devise_for :users, path: '', path_names: 
   { 
     sign_in: 'login', 
@@ -16,13 +15,15 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   #get 'tech-news', to: 'pages#tech_news'
 
-  get 'pages/contact'
+  #get 'pages/contact'
 
   resources :blogs do 
     member do 
       get :toggle_status
     end
   end
-
-root to: 'pages#home'
+  
+  mount ActionCable.server => '/cable'
+  
+  root to: 'pages#home'
 end
